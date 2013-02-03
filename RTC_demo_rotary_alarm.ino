62459fc6f9f3d443d2354c6880aef3db5e6d7564
jerwil <jerwil@gmail.com>
2013-02-03 02:44:59 -0600

    Working in alarm brach

--- RTC_demo_rotary_alarm.ino
+++ RTC_demo_rotary_alarm.ino
@@ -327,7 +327,7 @@ if (old_second >= 59){
   old_second = 0;
 }
 if (now_second > old_second){
-  Serial.print("Alarm!!!! This is testing using git gui");
+  Serial.print("Alarm!!!! This is testing using git gui. I checked out the alarm branch");
   Serial.println();
   old_second = now_second;
 }
